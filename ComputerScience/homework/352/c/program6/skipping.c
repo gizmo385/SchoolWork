f830480efb347bbcfd8d819714f9dbae472de148
Christopher(gizmo385) <cachapline8@gmail.com>
2014-11-07 16:30:05 -0700

    skipping.c now calls print_file after processing opts

--- ComputerScience/homework/352/c/program6/skipping.c
+++ ComputerScience/homework/352/c/program6/skipping.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 
 #include "processOpts.h"
+#include "printer.h"
 
 int main( int argc, char *argv[] ) {
     char *progName = argv[0];
 
     int flags[NUMBER_OF_FLAGS];
-    debug( "Testing\n");
     processOpts( argv, argc, progName, flags );
+
+    debug( "\nCurrent Flags:\n" );
+    debug( "--------------\n" );
+    debug( "Printing method: %s\n", flags[PRINTING_METHOD] == RECURSIVE ? "Recusive" : "Iterative" );
+    debug( "Printing method: %s\n", flags[PRINTING_DIRECTION] == PRINT_FORWARDS ? "Forwards" : "Backwards" );
+    debug( "Line skip: %d\n", flags[LINE_SKIP] );
+    debug( "--------------\n\n" );
+
+    argc -= optind;
+    argv += optind;
+
+    for( int i = 0; i < argc; i++ ) {
+        debug( "File argument: %s\n", argv[i] );
+        print_file( argv[i], flags );
+    }
 }
