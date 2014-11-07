dbd0ff1db32fbf1c3597ffe93fc9056ad66c394e
Christopher(gizmo385) <cachapline8@gmail.com>
2014-11-07 14:58:06 -0700

    Finished baseline for skipping.c

--- ComputerScience/homework/352/c/program6/skipping.c
+++ ComputerScience/homework/352/c/program6/skipping.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "processOpts.h"
 
 int main( int argc, char *argv[] ) {
-    printf( "HELLO WORLD");
+    char *progName = argv[0];
+
+    int flags[NUMBER_OF_FLAGS];
+    debug( "Testing\n");
+    processOpts( argv, argc, progName, flags );
 }
