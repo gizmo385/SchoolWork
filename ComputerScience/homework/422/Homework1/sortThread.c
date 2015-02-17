cf0a82b07bd63ea2b34f5d1f3802c36e85581b40
Christopher(gizmo385) <cachapline8@gmail.com>
2015-02-17 16:33:33 -0700

    Sort Thread almost working

--- ComputerScience/homework/422/Homework1/sortThread.c
+++ ComputerScience/homework/422/Homework1/sortThread.c
@@ -16,11 +16,16 @@ typedef struct SortingArguments {
 
     // The number of lines being sorted
     int size;
+
+    int id;
 } SortingArguments;
 
 typedef struct MergeArguments {
     // The location that firstLength is at in the original array
     int start;
+
+    // The array to memcpy into
+    char **lines;
 
     // The first array to merge
     char **first;
@@ -29,23 +34,20 @@ typedef struct MergeArguments {
     // The second array to merge
     char **second;
     int secondLength;
+
+    // The id for the thread running the worker
+    int id;
 } MergeArguments;
-
-typedef struct MergeReturn {
-    // The start of the original the merged blocks in the original array
-    int start;
-
-    // The size of the merged block
-    int size;
-
-    // The result of the merge
-    char **mergeResult;
-} MergeReturn;
 
 static inline void *SortingWorker( void *args ) {
     SortingArguments *sa = args;
     char **lines = sa->lines;
     int length = sa->size;
+    int id = sa->id;
+
+    printf( "Sorting worker:\n");
+    printf( "(id = %d) - Lines = %p\n", id, lines );
+    printf( "(id = %d) - Length = %d\n", id, length);
 
     quicksort( lines, length );
 
@@ -53,14 +55,37 @@ static inline void *SortingWorker( void *args ) {
 }
 
 static inline void *MergeWorker( void *args ) {
+    // Get the arguments
     MergeArguments *ma = args;
+    char **lines = ma->lines;
+    int start = ma->start;
+    int id = ma->id;
+
+    printf("\n\n(id = %d) - Values in first:\n", id);
+    for( int i = 0; i < ma->firstLength; i++ ) {
+        printf("(id = %d) - %s", id, ma->first[i]);
+    }
+
+    printf("\n---\n");
+
+    printf("(id = %d) - Values in second:\n", id);
+    for( int i = 0; i < ma->secondLength; i++ ) {
+        printf("(id = %d) - %s", id, ma->second[i]);
+    }
+
+    printf("\n---\n");
+
+    // Merge the lines
     char **res = merge(ma->first, ma->firstLength, ma->second, ma->secondLength);
 
-    MergeReturn *mr = malloc( sizeof(MergeReturn) );
-    mr->start = ma->start;
-    mr->mergeResult = res;
-    mr->size = ma->firstLength + ma->secondLength;
-    return mr;
+    printf( "(id = %d) - Merge results:\n", id);
+    for( int i = 0; i < ma->firstLength + ma->secondLength; i++ ) {
+        printf( "(id = %d) %s\n", id, res[i] );
+    }
+
+    memcpy( lines + start, res, ma->firstLength + ma->secondLength );
+
+    return NULL;
 }
 
 int main( int argc, char *argv[] ) {
@@ -95,11 +120,16 @@ int main( int argc, char *argv[] ) {
     // Create threads for sorting
     int numElements = length / numThreads;
     for( int id = 0; id < numThreads; id++ ) {
-        SortingArguments args;
-        args.lines = lines + (id * numElements);
-        args.size = numElements;
+        SortingArguments *args = malloc( sizeof(SortingArguments) );
+        args->lines = lines + (id * numElements);
+        args->size = numElements;
+        args->id = id;
 
-        int result = pthread_create( &workers[id], &attr, &SortingWorker, (void *)&args );
+        printf( "Creating sorting thread:\n");
+        printf( "(id = %d) - Lines = %p\n", id, lines + (id * numElements) );
+        printf( "(id = %d) - Length = %d\n", id, numElements);
+
+        int result = pthread_create( &workers[id], &attr, &SortingWorker, (void *)args );
 
         if( result != 0 ) {
             fprintf( stderr, "There was an error creating the sort thread id=%d!\n", id );
@@ -112,24 +142,45 @@ int main( int argc, char *argv[] ) {
         pthread_join(workers[id], NULL);
     }
 
+    printf("After sort, before merge:\n");
+    for( int i = 0; i < length; i++ ) {
+        printf( "%s", lines[i]);
+    }
+
+    printf("\n\n");
+
     // Create merge threads
     numThreads /= 2;
     for( ; numThreads > 0; numThreads /= 2 ) {
         pthread_t mergeWorkers[numThreads];
-        int numElements = length / numThreads;
+        int numElements = length / (numThreads * 2);
+        int remainder = length % (numThreads * 2);
 
         // Create merge workers
         for( int id = 0; id < numThreads; id++ ) {
             // Create the arguments for the merge step
-            MergeArguments args;
-            args.start = 2 * id * numElements;
-            args.first = lines + (2 * id * numElements);
-            args.firstLength = numElements;
-            args.second = lines + (2 * (id + 1) * numElements);
-            args.secondLength = numElements;
+            MergeArguments *args = malloc(sizeof(MergeArguments));
+            args->start = id * numElements;
+            args->lines = lines;
+            args->first = lines + (id * numElements);
+            args->firstLength = numElements;
+            args->second = lines + ((id + 1) * numElements);
+            args->secondLength = numElements;
+
+            if( (id + 1) == numThreads ) {
+                args->secondLength += remainder;
+            }
+
+            printf( "Creating merging thread:\n");
+            printf( "(id = %d) - Start = %d\n", id, args->start );
+            printf( "(id = %d) - Lines = %p\n", id, args->lines );
+            printf( "(id = %d) - First = %p\n", id, args->first );
+            printf( "(id = %d) - FirstLength = %d\n", id, args->firstLength );
+            printf( "(id = %d) - Second = %p\n", id, args->second );
+            printf( "(id = %d) - secondLength = %d\n", id, args->secondLength );
 
             // Create a thread to merge 2 array segments
-            int result = pthread_create( &mergeWorkers[id], &attr, &MergeWorker, (void *)&args );
+            int result = pthread_create( &mergeWorkers[id], &attr, &MergeWorker, (void *)args );
 
             if( result != 0 ) {
                 fprintf( stderr, "There was an error creating the merge thread id=%d!\n", id );
@@ -139,17 +190,7 @@ int main( int argc, char *argv[] ) {
 
         // Join merge workers
         for( int id = 0; id < numThreads; id++ ) {
-            // Get the result from the thread
-            void *mergeResult;
-            pthread_join(workers[id], &mergeResult);
-
-            // Get the content of the return
-            MergeReturn *mr = mergeResult;
-            int mrStart = mr->size;
-            char **res = mr->mergeResult;
-            int size = mr->size;
-
-            memcpy(lines + mrStart, res, size);
+            pthread_join(mergeWorkers[id], NULL);
         }
     }
 
@@ -158,6 +199,7 @@ int main( int argc, char *argv[] ) {
     // Print the lines and the information about the sorted data
     int seconds = (int)(microseconds / CLOCKS_PER_SEC);
 
+    printf("\nAfter merge:\n");
     for( int i = 0; i < length; i++ ) {
         printf("%s", lines[i]);
     }
