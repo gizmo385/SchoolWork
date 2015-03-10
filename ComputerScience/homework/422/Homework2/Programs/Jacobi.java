1a567ce412ef1e7db00a238d688cd3f2979d163c
Christopher(gizmo385) <cachapline8@gmail.com>
2015-03-10 12:00:01 -0700

    Java version no longer recreates threads

--- ComputerScience/homework/422/Homework2/Programs/Jacobi.java
+++ ComputerScience/homework/422/Homework2/Programs/Jacobi.java
@@ -7,12 +7,23 @@ import java.io.IOException;
 
 import java.util.Arrays;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.CountDownLatch;
 
 public class Jacobi {
 
     private static double[][] grid, new_grid;
     private static double maxDifference;
+    private static int numThreads, numberOfIterations = 1;
+
+    /*
+     * Controls the creation of threads, ensuring that the main thread won't block until all threads
+     * have been acquired and acquired thread
+     */
+    private static Semaphore creationSemaphore;
+
+    // The largest difference between successive iterations that we will terminate on
+    private static final double EPSILON = 0.1;
+
+    private static boolean finished = false;
 
     /**
      * Performs Jacobi iteration using a grid of a specified size and parsing it with
@@ -24,9 +35,6 @@ public class Jacobi {
      * bottom exteriors respectively.
      */
     public static void main( String[] args ) {
-        // The largest difference between successive iterations that we will terminate on
-        final double EPSILON = 0.1;
-
         // Ensure that the user has entered the correct number of arguments
         if( args.length < 2 ) {
             System.err.println("Usage: java Jacobi <gridSize> <numThreads>");
@@ -41,10 +49,9 @@ public class Jacobi {
 
         // Read in the command line arguments
         int gridSize = Integer.parseInt( args[0] );
-        int numThreads = Integer.parseInt( args[1] );
+        numThreads = Integer.parseInt( args[1] );
 
         // Iterations and batch size
-        int numberOfIterations = 0;
         int rowsPerThread = gridSize / numThreads;
         int leftover = gridSize % numThreads;
 
@@ -79,51 +86,39 @@ public class Jacobi {
         // Semaphore for interacting with maxDifference
         Semaphore maxSem = new Semaphore(1);
         Semaphore threadsSemaphore = new Semaphore(numThreads);
+        Semaphore iterationSemaphore = new Semaphore(numThreads);
 
         // Perform the iterations
-        for( ; ; numberOfIterations++ ) {
-            maxDifference = 0.0;
-
-            // Create threads to perform the iterations
-            for( int thread = 0; thread < numThreads; thread++ ) {
-                // Acquire a permit from the semaphore for each thread that we're creating
-                try {
-                    threadsSemaphore.acquire();
-                } catch( InterruptedException ie ) {
-                    System.err.println("interrupted while attempting to acquire semaphore!");
-                    System.exit(1);
-                }
-
-                // Create the worker
-                WorkerThread worker;
-                int startingRow = (thread * rowsPerThread) + 1;
-                int endingRow = startingRow + rowsPerThread;
-
-                if( (thread + 1) == numThreads ) {
-                    worker = new WorkerThread(gridSize, startingRow, endingRow + leftover, maxSem,
-                            threadsSemaphore);
-                } else {
-                    worker = new WorkerThread(gridSize, startingRow, endingRow, maxSem,
-                            threadsSemaphore);
-                }
-
-                // Start the worker
-                worker.start();
+        maxDifference = 0.0;
+        // Create threads to perform the iterations
+        for( int thread = 0; thread < numThreads; thread++ ) {
+            try {
+                threadsSemaphore.acquire();
+            } catch( InterruptedException ie ) {
+                System.err.printf( "Error while creating thread %d! Exiting...\n", thread );
+                System.exit(1);
             }
 
-            // Block until the counter reaches zero
-            while( threadsSemaphore.availablePermits() != numThreads ) {
-                // Spin
+            // Create the worker
+            WorkerThread worker;
+            int startingRow = (thread * rowsPerThread) + 1;
+            int endingRow = startingRow + rowsPerThread;
+
+            if( (thread + 1) == numThreads ) {
+                worker = new WorkerThread(thread, gridSize, startingRow, endingRow + leftover,
+                        maxSem, threadsSemaphore, iterationSemaphore);
+            } else {
+                worker = new WorkerThread(thread,gridSize, startingRow, endingRow, maxSem,
+                        threadsSemaphore, iterationSemaphore);
             }
 
-            // Determine if we've completed the iteration
-            if( maxDifference < EPSILON ) {
-                break;
-            }
+            // Start the worker
+            worker.start();
+        }
 
-            for( int i = 0; i < grid.length; i++ ) {
-                System.arraycopy(new_grid[i], 0, grid[i], 0, grid.length);
-            }
+        // Block until the counter reaches zero
+        while( threadsSemaphore.availablePermits() != numThreads ) {
+            // Spin
         }
 
         // Stop the timer
@@ -181,25 +176,29 @@ public class Jacobi {
      */
     private static class WorkerThread extends Thread {
 
-        private int gridSize, firstRow, lastRow;
-        private Semaphore maxSem, threadsSemaphore;
+        private int gridSize, firstRow, lastRow, threadId;
+        private Semaphore maxSem, threadsSemaphore, iterationSemaphore;
 
         /**
          * Creates a thread which will perform Jacobi iterations.
          *
+         * @param threadId          the integer identifier for this thread.
          * @param gridSize          the size of the interior grid that is being iterated over.
          * @param firstRow          the index of the first row being operated on.
          * @param lastRow           the index of the last row being operated on.
          * @param maxSem            the semaphore that controls access to maxDifference.
          * @param threadsSemaphore  the semaphore that counts the number of threads active.
+         * @param iterSemaphore     the semaphore for the threads performing their iterations
          */
-        public WorkerThread(int gridSize, int firstRow, int lastRow, Semaphore maxSem,
-                Semaphore threadsSemaphore) {
+        public WorkerThread(int thread, int gridSize, int firstRow, int lastRow, Semaphore maxSem,
+                Semaphore threadsSemaphore, Semaphore iterSemaphore ) {
+            this.threadId = thread;
             this.gridSize = gridSize;
             this.firstRow = firstRow;
             this.lastRow = lastRow;
             this.maxSem = maxSem;
             this.threadsSemaphore = threadsSemaphore;
+            this.iterationSemaphore = iterSemaphore;
         }
 
         /**
@@ -208,33 +207,86 @@ public class Jacobi {
          * global distance.
          */
         @Override public void run() {
-            // Calculate a subset of the rows
-            for( int row = firstRow; row < lastRow; row++ ) {
-                for( int column = 1; column <= gridSize; column++ ) {
-                    new_grid[row][column] = (grid[row - 1][column] + grid[row + 1][column]
-                            + grid[row][column - 1] + grid[row][column + 1]) * 0.25;
+            while(!finished) {
+                try {
+                    //while( iterationSemaphore.availablePermits() != numThreads ) {
+                        //Thread.sleep(1);
+                    //}
+                    iterationSemaphore.acquire();
+
+                    //System.out.printf("[Thread %d] Acquired iteration permit\n", threadId);
+                } catch( InterruptedException ie ) {
+                    System.err.printf("Interrupted with acquiring semaphore! Exiting...\n");
+                    System.exit(1);
+                }
+
+                // Calculate a subset of the rows
+                for( int row = firstRow; row < lastRow; row++ ) {
+                    for( int column = 1; column <= gridSize; column++ ) {
+                        new_grid[row][column] = (grid[row - 1][column] + grid[row + 1][column]
+                                + grid[row][column - 1] + grid[row][column + 1]) * 0.25;
+                    }
+                }
+
+                // Compute the maximal difference amongst these rows
+                double localMax = 0.0;
+                for( int row = firstRow; row < lastRow; row++ ) {
+                    for( int column = 1; column <= gridSize; column++ ) {
+                        localMax = max(localMax, abs(new_grid[row][column] - grid[row][column]));
+                    }
+                }
+
+                // Determine if the local max is larger than the global max
+                try {
+                    maxSem.acquire();
+                    maxDifference = max(maxDifference, localMax);
+                    maxSem.release();
+                } catch( InterruptedException ie ) {
+                    System.err.println("Max difference calculation interrupted! Exiting!");
+                    System.exit(1);
+                }
+
+                // Wait for the other threads to finish
+                try {
+                    // The first thread will update global state
+                    if( threadId == 0 ) {
+                        // Wait for the other threads to finish
+                        while( iterationSemaphore.availablePermits() != (numThreads - 1) ) {
+                            Thread.sleep(1);
+                        }
+
+                        // Check if we've finished iterating
+                        if( maxDifference < EPSILON ) {
+                            finished = true;
+                        } else {
+                            // If haven't, update the state
+                            maxDifference = 0.0;
+                            numberOfIterations++;
+                        }
+
+                        iterationSemaphore.release();
+                        while( iterationSemaphore.availablePermits() != numThreads ) {
+                        }
+                    } else {
+                        iterationSemaphore.release();
+
+                        while( iterationSemaphore.availablePermits() != numThreads ) {
+                        }
+                    }
+
+                    // Update our grid if we haven't finished
+                    if( !finished ) {
+                        for( int i = firstRow; i < lastRow; i++ ) {
+                            System.arraycopy(new_grid[i], 0, grid[i], 0, grid.length);
+                        }
+                    }
+
+                } catch( InterruptedException ie ) {
+                    System.err.printf( "Error while waiting at barrier in thread %d\n", threadId );
+                    System.exit(1);
                 }
             }
 
-            // Compute the maximal difference amongst these rows
-            double localMax = 0.0;
-            for( int row = firstRow; row < lastRow; row++ ) {
-                for( int column = 1; column <= gridSize; column++ ) {
-                    localMax = max(localMax, abs(new_grid[row][column] - grid[row][column]));
-                }
-            }
-
-            // Determine if the local max is larger than the global max
-            try {
-                maxSem.acquire();
-                maxDifference = max(maxDifference, localMax);
-                maxSem.release();
-            } catch( InterruptedException ie ) {
-                System.err.println("Max difference calculation interrupted! Exiting!");
-                System.exit(1);
-            }
-
-            // Release a permit from the threads semaphore
             threadsSemaphore.release();
         }
     }
