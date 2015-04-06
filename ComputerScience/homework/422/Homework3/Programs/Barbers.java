e40e8f5d9e95021877d0c53fb43774ea38ec74ea
Christopher Chapline <cachapline8@gmail.com>
2015-04-05 17:40:45 -0700

    Stub file for Barbers

---
+++ ComputerScience/homework/422/Homework3/Programs/Barbers.java
@@ -0,0 +1,8 @@
+public class Barbers {
+    public static void main( String[] args ) {
+        // Read command-line arguments
+        int numberOfCustomers = Integer.parseInt(args[0]);
+        int haircutTime = Integer.parseInt(args[1]);
+        int arrivalInterval = Integer.parseInt(args[2]);
+    }
+}
