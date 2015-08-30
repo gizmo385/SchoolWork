f534ccfb7b2f425857c4fcaec7b2468af3af34e5
Christopher Chapline <cachapline8@gmail.com>
2015-08-30 16:13:33 -0700

    483 Assignment1: Searching index with and

--- ComputerScience/483/assignment1/index/src/index/index.clj
+++ ComputerScience/483/assignment1/index/src/index/index.clj
@@ -40,3 +40,44 @@
                                       (map sort (vals index))))))
 
 (clojure.pprint/pprint (inverted-index test-documents))
+
+(defmulti search-index-op
+  "Implements different search operations"
+  (fn [op index term1-results term2-results] op))
+
+(defmethod search-index-op :and [_ index term1-results term2-results]
+  (loop [results []
+         term1-results term1-results
+         term2-results term2-results]
+    (if (and (not-empty term1-results) (not-empty term2-results))
+      (let [term1-first (first term1-results)
+            term2-first (first term2-results)
+            term1-shorter (< (count term1-results) (count term2-results))]
+        (if (= term1-first term2-first)
+          (recur (conj results term1-first)
+                 (rest term1-results)
+                 (rest term2-results))
+          (recur results
+                 (if term1-shorter term1-results (rest term1-results))
+                 (if term1-shorter (rest term2-results) term2-results))))
+      results)))
+
+(defn search-index [op {:keys [documents index]} terms]
+  (if (< 2 (count terms))
+    (->> terms
+         (map (partial get index))
+         (sort-by count)
+         (map (partial get documents)))
+    (->> terms
+         (map (partial get index))
+         (sort-by count)
+         (reduce (partial search-index-op op index))
+         (map (partial get documents)))))
+
+(comment
+  (let [index (inverted-index test-documents)]
+    (println (map slurp test-documents))
+    (println (search-index :and index ["drug" "schizophrenia"]))
+    (println (search-index :and index ["schizophrenia"]))
+    (println (search-index :and index ["approach"]))
+    ))
