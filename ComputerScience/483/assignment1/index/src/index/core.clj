42981aec2ecd564acf99ad3b62b13044cf76e370
Christopher Chapline <cachapline8@gmail.com>
2015-09-01 17:39:41 -0700

    483 Assignment1: Main method implementation

--- ComputerScience/483/assignment1/index/src/index/core.clj
+++ ComputerScience/483/assignment1/index/src/index/core.clj
@@ -1,10 +1,20 @@
 (ns index.core
-  (:require [index.index :refer [search-index inverted-index]])
+  (:require [index.index :refer [search-index inverted-index]]
+            [clojure.string :refer [split-lines trim]])
   (:gen-class))
 
 (defn -main
+  "First we construct the inverted index. The first argument to the program should be a filename
+   where each line in the file is a string of text, which will be treated as a document. Arguments
+   supplied after this will be assumed to be your search query terms."
   [& args]
-  (let [index (inverted-index args)]
-    (println (search-index :and index ["drug" "schizophrenia" "approach"]))
-    )
-  )
+  (let [index (->> args
+                   (first)
+                   (slurp)
+                   (split-lines)
+                   (map trim)
+                   (inverted-index))
+        query (if (not-empty (rest args))
+                (rest args)
+                (line-seq (java.io.BufferedReader. *in*)))]
+    (println (search-index index :and query))))
