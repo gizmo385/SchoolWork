f71f6a47898630a49220a175f6f7185e3de62615
Christopher Chapline <cachapline8@gmail.com>
2015-09-04 11:59:40 -0700

    483 Assignment1: Add better query prompt

--- ComputerScience/483/assignment1/index/src/index/core.clj
+++ ComputerScience/483/assignment1/index/src/index/core.clj
@@ -3,22 +3,36 @@
             [clojure.string :refer [split split-lines trim]])
   (:gen-class))
 
+(defn parse-query [query]
+  (map trim (split query #"\s+")))
+
+(defn prompt [prompt-text]
+  (flush)
+  (print prompt-text)
+  (flush)
+  (read-line))
+
+(defn- start-query-prompt [index]
+  (loop [query-text (prompt ">>> ")]
+    (when (not-empty query-text)
+      (->> query-text
+           parse-query
+           (search-index index :and)
+           println)
+      (recur (prompt ">>> ")))))
+
 (defn -main
   "First we construct the inverted index. The first argument to the program should be a filename
-   where each line in the file is a string of text, which will be treated as a document. Arguments
-   supplied after this will be assumed to be your search query terms."
+   where each line in the file is a string of text, which will be treated as a document. Arguments supplied after this will be assumed to be your search query terms."
   [& args]
   (let [index (->> args
                    first              ; The first element is our file containing documents
                    slurp              ; We should read the file
                    split-lines        ; Capture the lines
                    (map trim)         ; Trim each line
-                   inverted-index)    ; And construct our inverted index based on the lines
-        query (if (not-empty (rest args))
-                (rest args)
-                (->> *in*
-                     java.io.BufferedReader.  ; We should open a reader on stdin
-                     line-seq                 ; Get a sequence of lines from that reader
-                     (map #(split %1 #"\s+")) ; Split the line based on spaces
-                     flatten))]               ; And then flatten
-    (println (search-index index :and query))))
+                   inverted-index)]   ; And construct our inverted index based on the lines
+    (if (not-empty (rest args))
+      (println (search-index index :and (rest args)))
+      (start-query-prompt index))))
+
+
