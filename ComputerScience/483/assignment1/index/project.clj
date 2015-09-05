d0ef53c3a654a22c04f95d44885775d7b9aa34f0
Christopher Chapline <cachapline8@gmail.com>
2015-09-05 00:09:36 -0700

    483 Assignment1: Update project description

--- ComputerScience/483/assignment1/index/project.clj
+++ ComputerScience/483/assignment1/index/project.clj
@@ -1,6 +1,5 @@
-(defproject index "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject index "1.0"
+  :description "An implementation of an inverted index in Clojure for Assignment 1 of CSc 483."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
