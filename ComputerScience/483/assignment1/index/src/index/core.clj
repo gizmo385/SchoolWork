e734d97a48f64cb7638ea43b37daaa3356a0eaf7
Christopher Chapline <cachapline8@gmail.com>
2015-09-05 00:11:30 -0700

    483 Assignment1: Prompt is dynamic variable
    Converted the prompt text to a dynamic variable. This allows for code like this to exist:
    
    (binding [*prompt* ">> "]
      (prompt))
    
    to dynamically redefine the prompt

--- ComputerScience/483/assignment1/index/src/index/core.clj
+++ ComputerScience/483/assignment1/index/src/index/core.clj
@@ -3,33 +3,40 @@
             [clojure.string :refer [split split-lines trim]])
   (:gen-class))
 
+(def ^:dynamic *prompt*
+  "This is the prompt text that will be printed while asking for user queries."
+  ">>> ")
+
 (defn parse-query
   "Parses the query so that it can be sent to the index"
   [query]
   (split query #"\s+"))
 
 (defn prompt
-  "Writes the desired prompt to stdout and then reads input from stdin"
-  [prompt-text]
-  (print prompt-text)
+  "Writes the prompt to stdout and then reads input from stdin. This prompt is specified in the by
+   the *prompt* variable."
+  []
+  (print *prompt*)
   (flush)
   (read-line))
 
 (defn- start-query-prompt
   "Starts a prompt that listens for queries and then makes those queries against the supplied
    index."
-  [index prompt-text]
-  (loop [query-text (prompt prompt-text)]
+  [index]
+  (loop [query-text (prompt)]
     (when (not-empty query-text)
       (->> query-text
            parse-query
            (search-index index :and)
            println)
-      (recur (prompt prompt-text)))))
+      (recur (prompt)))))
 
 (defn -main
   "First we construct the inverted index. The first argument to the program should be a filename
-   where each line in the file is a string of text, which will be treated as a document. Arguments supplied after this will be assumed to be your search query terms."
+   where each line in the file is a string of text, which will be treated as a document.  Arguments
+   supplied after this will be assumed to be your search query terms. Arguments supplied to this
+   program will be by assuming an implicit AND exists between elements"
   [& args]
   (let [index (->> args
                    first              ; The first element is our file containing documents
@@ -39,4 +46,4 @@
                    inverted-index)]   ; And construct our inverted index based on the lines
     (if (not-empty (rest args))
       (println (search-index index :and (rest args)))
-      (start-query-prompt index ">>> "))))
+      (start-query-prompt index))))
