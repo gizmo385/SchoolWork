c101907e09ff590796c2ec308bb6f4007c5f38bd
Christopher Chapline <cachapline8@gmail.com>
2015-10-29 11:43:41 -0700

    483 3: Add basic REPL loop in main

--- ComputerScience/483/assignment3/index/src/index/core.clj
+++ ComputerScience/483/assignment3/index/src/index/core.clj
@@ -1,6 +1,23 @@
-(ns index.core)
+(ns index.core
+  (:require [clojure.string :refer [join]]
+            (index.lucene [core :refer [create-index search-index]]))
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:dynamic *prompt*
+  "This is the prompt text that will be printed while asking for user queries."
+  "Enter your query: ")
+
+(defn- prompt
+  "Writes the prompt to stdout and then reads input from stdin. This prompt is specified in the by
+   the *prompt* variable."
+  []
+  (print *prompt*)
+  (flush)
+  (read-line))
+
+(defn -main [& filenames]
+  (let [index (apply create-index filenames)]
+    (loop [query (prompt)]
+      (let [results (search-index index query)]
+        (println (join ", "(map #(.get % "filename") results))))
+      (recur (prompt)))))
