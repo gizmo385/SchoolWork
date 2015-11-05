fa6621de662fbede7e90f011f1e46cc2f61140d1
Christopher Chapline <cachapline8@gmail.com>
2015-11-04 18:32:08 -0700

    483 4: Rewrite core with wrapper

--- ComputerScience/483/assignment4/index/src/index/core.clj
+++ ComputerScience/483/assignment4/index/src/index/core.clj
@@ -1,19 +1,11 @@
 (ns index.core
-  (:require [clojure.string :refer [join split split-lines]])
-  (:import [org.apache.lucene.analysis.standard StandardAnalyzer]
-           [org.apache.lucene.util Version]
-           [org.apache.lucene.store RAMDirectory Directory]
-           [org.apache.lucene.index IndexReader IndexWriter IndexWriterConfig DirectoryReader]
-           [org.apache.lucene.document Field$Store StringField TextField Document]
-           [org.apache.lucene.queryparser.classic QueryParser]
-           [org.apache.lucene.search IndexSearcher Query TopScoreDocCollector ScoreDoc Sort]
-           [java.io FileReader StringReader])
+  (:require [clojure.string :refer [join split split-lines]]
+            [clojure.java.io :refer [as-file]]
+            [clojure.pprint :refer [pprint]]
+            [index.lucene :refer [create-index search-index]])
   (:gen-class))
 
-;; Some global settings
-(def lucene_version Version/LUCENE_5_3_1)
-(def text-analyzer (StandardAnalyzer.))
-(def ^:dynamic *hits-per-page* 10)
+;; Some utility functions
 (def ^:dynamic *prompt*
   "This is the prompt text that will be printed while asking for user queries."
   "Enter your query: ")
@@ -26,44 +18,20 @@
   (flush)
   (read-line))
 
-;; Indexing and searching
-(defn create-document
-  "Creates a document object based on the contents if a file."
-  [document-name contents]
-  (doto (Document.)
-    (.add (StringField. "document-name" document-name Field$Store/YES))
-    (.add (TextField. "contents" (StringReader. contents)))))
-
-(defn add-to-index [writer document-name contents]
-  (.addDocument writer (create-document document-name contents)))
-
-(defn search-index
-  "Searches the index and returns the documents that match the search."
-  [index query & {:keys [field] :or {field "contents"}}]
-  (let [query  (.parse (QueryParser. field text-analyzer) query)
-        reader        (DirectoryReader/open index)
-        searcher      (IndexSearcher. reader)
-        collector     (TopScoreDocCollector/create *hits-per-page*)]
-    (doto searcher
-      (.search query collector))
-    (for [result (.scoreDocs (.topDocs collector))]
-      [(.score result) (.doc searcher (.doc result))])))
+(defn file-parser [line]
+  (let [[document-name & contents] (split line #":")]
+    (printf "Document name: %s\n" document-name)
+    (printf "Contents: %s\n" (apply str contents))
+    [{:type :string :name "document-name" :contents document-name :store true}
+     {:type :text :name "contents" :contents (apply str contents) :store false}]))
 
 ;; Main program loop
 (defn -main [& filenames]
-  (let [index (RAMDirectory.)]
-    (with-open [writer (IndexWriter. index (IndexWriterConfig. text-analyzer))]
-      (doto writer
-        (add-to-index "Document #1" "information retrieval is the most awesome class I ever took.")
-        (add-to-index "Document #2" "the retrieval of private information from your emails is a job that the NSA loves.")
-        (add-to-index "Document #3" "in the school of information you can learn about data science.")
-        (add-to-index "Document #4" "the labrador retriever is a great dog.")))
+  (let [index (create-index (flatten (map (comp split-lines slurp) filenames)) file-parser)]
     (try
       (loop [query (prompt)]
-        (let [results (search-index index query)
+        (let [results (search-index index query "contents")
               num-results (count results)]
-          (printf "Results found: %d\n" num-results)
-          (println (join ", " (for [[score document] results]
-                                (format "%s(%f)" (.get document "document-name") score)))))
+          (printf "Results found: %d\n" num-results))
         (recur (prompt)))
       (catch NullPointerException _ (do (println) (System/exit 0))))))
