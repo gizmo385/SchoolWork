a8897a9a8397108cb13fc586215cfef2928f5179
Christopher Chapline <cachapline8@gmail.com>
2015-12-06 13:48:35 -0700

    483 4: Retrieving documents, ranks broken

--- ComputerScience/483/assignment4/index/src/main/java/edu/arizona/cachapline8/LanguageModel.java
+++ ComputerScience/483/assignment4/index/src/main/java/edu/arizona/cachapline8/LanguageModel.java
@@ -9,21 +9,22 @@ import edu.arizona.sista.processors.*;
 import edu.arizona.sista.processors.fastnlp.FastNLPProcessor;
 
 public class LanguageModel {
+    // Determines considerations between the document model and collection model
+    private static final double LAMBDA = 0.5;
+
     // Set up the processor
     private static Processor proc = new FastNLPProcessor(true, false, false);
 
     // { term : { documentName : frequency } }
     private static Map<String, Map<String, Integer>> termMaps = new HashMap<>();
 
+    // { term : global term count }
+    private static Map<String, Integer> globalTermCounts = new HashMap<>();
+
     // { documentName : length }
     private static Map<String, Integer> documentLengths = new HashMap<>();
 
-    public static double maximumLiklihoodEstimate(String term, String documentName) {
-        Map<String, Integer> termFrequencies = termMaps.getOrDefault(term, new HashMap<>());
-        int termFrequency = termFrequencies.getOrDefault(documentName, 0);
-
-        return termFrequency / documentLengths.getOrDefault(documentName, 1);
-    }
+    private static int numberOfTokens = 0;
 
     public static void parseDocument(String documentText) {
         int colon = documentText.indexOf(':');
@@ -40,11 +41,72 @@ public class LanguageModel {
                 termMap.put(documentName, termMap.getOrDefault(documentName, 0) + 1);
                 termMaps.put(term, termMap);
 
+                globalTermCounts.put(term, globalTermCounts.getOrDefault(term, 0) + 1);
+
                 documentLength += 1;
             }
         }
 
+        numberOfTokens += documentLength;
         documentLengths.put(documentName, documentLength);
+    }
+
+    public static double handleQueryTermInDocument(String term, String documentName) {
+        Map<String, Integer> termMap = termMaps.getOrDefault(term, new HashMap<String, Integer>());
+
+        // Calculate the value for the document
+        int documentFreq = termMap.getOrDefault(documentName, 0);
+        double documentTermValue = documentFreq / documentLengths.getOrDefault(documentName, 1);
+
+        // Calculate the value for the collection
+        double collectionTermValue = globalTermCounts.get(term) / numberOfTokens;
+
+        // Smooth the document and collection values
+        return (LAMBDA * documentTermValue) + ((1 - LAMBDA) * collectionTermValue);
+    }
+
+    public static double handleQueryInDocument(String query, String documentName) {
+        double ranking = 1;
+
+        for(String term : query.split("\\s+")) {
+            ranking *= handleQueryTermInDocument(term, documentName);
+        }
+
+        return ranking;
+    }
+
+    public static Map<String, Double> rankDocuments(String query, Set<String> documents) {
+        Map<String, Double> rankings = new HashMap<>();
+
+        for(String documentName : documents) {
+            rankings.put(documentName, handleQueryInDocument(query, documentName));
+        }
+
+        return rankings;
+    }
+
+    public static Set<String> getDocumentList(String query) {
+        Set<String> documents = new TreeSet<>();
+
+        for(String term : query.split("\\s+")) {
+            documents.addAll(termMaps.getOrDefault(term, new HashMap<String, Integer>()).keySet());
+        }
+
+        return documents;
+    }
+
+    public static void executeQuery(String query) {
+        Set<String> documents = getDocumentList(query);
+        Map<String, Double> rankings = rankDocuments(query, documents);
+
+        // Sort the documents based on the rankings
+        List<String> byRank = new ArrayList<>(documents);
+        Collections.sort(byRank, (d1, d2) -> Double.compare(rankings.get(d1), rankings.get(d2)));
+
+        // Print the documents
+        for(String documentName : byRank) {
+            System.out.printf("%s (%f)\n", documentName, rankings.getOrDefault(documentName, 0.0));
+        }
     }
 
     public static void main(String [] filenames) throws Exception {
@@ -53,7 +115,17 @@ public class LanguageModel {
             Files.lines(Paths.get(filename)).forEach(LanguageModel::parseDocument);
         }
 
-        System.out.println("Terms:");
-        termMaps.keySet().stream().forEach(System.out::println);
+        // Read user queries
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            System.out.print("Enter a query (exit to quit): ");
+            String query = input.nextLine();
+
+            if("exit".equals(query)) {
+                break;
+            } else {
+                executeQuery(query);
+            }
+        }
     }
 }
