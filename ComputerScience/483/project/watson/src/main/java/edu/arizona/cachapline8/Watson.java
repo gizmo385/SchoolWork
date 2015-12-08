a7c4d2a60d6048c4d94dc18bcb0c443015e4e478
Christopher Chapline <cachapline8@gmail.com>
2015-12-08 01:38:06 -0700

    483 4: Working on Watson project. Results subpar

--- ComputerScience/483/project/watson/src/main/java/edu/arizona/cachapline8/Watson.java
+++ ComputerScience/483/project/watson/src/main/java/edu/arizona/cachapline8/Watson.java
@@ -14,11 +14,13 @@ import org.apache.lucene.index.*;
 import org.apache.lucene.document.*;
 import org.apache.lucene.queryparser.classic.*;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.*;
 
 public class Watson {
 
     private final static Pattern articleTitle = Pattern.compile("^\\[\\[(?<title>.+)\\]\\]$");
     private final static int HITS_PER_PAGE = 1;
+    private final static String INDEX_FILENAME = "./watson_directory.dat";
 
     private static List<String> lines = new ArrayList<>();
     private static List<String> categories = new ArrayList<>();
@@ -43,6 +45,7 @@ public class Watson {
             // A document out of the previous article
             if(currentArticleTitle != null) {
                 Document d = createDocument();
+                System.out.printf("%d: %s\n", numberOfArticles, currentArticleTitle);
                 numberOfArticles++;
 
                 try {
@@ -81,43 +84,47 @@ public class Watson {
         StandardAnalyzer analyzer = new StandardAnalyzer();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
-        System.out.println("Parsing articles...");
 
         try {
-            Directory index = new SimpleFSDirectory(Paths.get("./watson_directory.dat"));
-            final IndexWriter writer = new IndexWriter(index, config);
-            Consumer<String> lineHandler = line -> handleLine(writer, line);
+            File indexFile = new File(INDEX_FILENAME);
+            Directory index;
 
-            // Parse the articles
-            for(String directory : directories) {
-                try {
-                    Files.list(Paths.get(directory)).forEach(f -> parseWikiDocument(lineHandler, f));
-                } catch(IOException ioe) {
-                    System.err.printf("Error while attempting to parse directory(%s): %s\n", directory,
-                            ioe);
+            if(indexFile.exists()) {
+                System.out.println("Directory already exists. Loading...");
+                index = new SimpleFSDirectory(Paths.get(INDEX_FILENAME));
+            } else {
+                System.out.println("Parsing articles...");
+                index = new SimpleFSDirectory(Paths.get(INDEX_FILENAME));
+                final IndexWriter writer = new IndexWriter(index, config);
+                Consumer<String> lineHandler = line -> handleLine(writer, line);
+
+                // Parse the articles
+                for(String directory : directories) {
+                    try {
+                        Files.list(Paths.get(directory)).forEach(f -> parseWikiDocument(lineHandler, f));
+                    } catch(IOException ioe) {
+                        System.err.printf("Error while attempting to parse directory(%s): %s\n", directory,
+                                ioe);
+                    }
                 }
+
+                // Close the index writer
+                writer.close();
+                System.out.println("Finished parsing articles.");
             }
 
-            // Close the index writer
-            writer.close();
-
-            System.out.println("Finished parsing articles.");
 
             // Set up the query parser
             String[] fields = { "article_title", "contents", "categories" };
-            Map<String, Float> boosts = new HashMap<>();
-            boosts.put("article_title", 5f);
-            boosts.put("contents", 2f);
-            boosts.put("categories", 8f);
             MultiFieldQueryParser queryParser = new MultiFieldQueryParser(fields, analyzer);
 
             // Read in user queries
             Scanner input = new Scanner(System.in);
             while(true) {
-                System.out.print("Enter your query: ");
-                String query = input.nextLine();
                 System.out.print("Enter the question category: ");
                 String category = input.nextLine();
+                System.out.print("Enter your query: ");
+                String query = input.nextLine();
 
                 if("exit".equals(query)) {
                     break;
@@ -128,6 +135,7 @@ public class Watson {
                 try {
                     IndexReader reader = DirectoryReader.open(index);
                     IndexSearcher searcher = new IndexSearcher(reader);
+                    searcher.setSimilarity(new BM25Similarity());
                     TopScoreDocCollector collector = TopScoreDocCollector.create(HITS_PER_PAGE);
                     Query q = queryParser.parse(queries, fields, analyzer);
                     searcher.search(q, collector);
@@ -140,11 +148,14 @@ public class Watson {
                         System.out.printf("Most likely: %s\n", d.get("article_title"));
                     }
                 } catch(ParseException pe) {
-                    System.err.printf("Error parsing query \"%s\": %s\n", query, pe);
+                    System.err.printf("Error parsing query \"%s\"!\n", query);
                 }
             }
         } catch(IOException ioe) {
             System.err.printf("Error openning directory: %s\n", "./watson_directory.dat");
+        } catch(NoSuchElementException nsfe) {
+            System.out.println();
+            // Do nothing - END OF INPUT
         }
     }
 }
